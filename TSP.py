4aa4bdb698756b0b0b94eda672f4d77c252ce415
Juan <j.florez-velasquez@mycit.ie>
2019-09-29 12:24:07 +0200

    TSP v2 correct one

--- TSP.py
+++ TSP.py
@@ -30,29 +30,46 @@ def distance(origen, destination):
     """
     return round( sqrt( (origen[0] - destination[0])**2 + 
                      (origen[1] -destination[1])**2) )
+
+
+def nearest_node(index, dictionary, visited_list):
+    """ returns the nearest node to the dictionary[index] in the
+        dictionary
+    """
+    min_dis = 98888888888
+    closest_node = index
+    for d_node in dictionary:
+        if d_node == index:
+            continue
+        elif d_node in visited_list:
+            continue
+        
+        current_distance = distance(dictionary[index], dictionary[d_node])
+        if 0 < current_distance < min_dis:
+            min_dis = current_distance
+            closest_node = d_node
     
+    return min_dis, closest_node
+
+        
+
 
 def tsp_impl(input_dir, output_list, start=1):
     """ takes input_dir, and gives a sorted list with the nodes visited in 
     order """
     
     logging.info("tsp_impl with starts")
-    output_list.insert(0,start)
+    output_list.append(start)
+    cycle_lenght = 0
+    o_point = start
     
+    while(len(output_list) < len(input_dir)):
         
+        distance, index = nearest_node(o_point, input_dir, output_list)
+       
+        output_list.append(index)
+        cycle_lenght += distance
+        o_point = index
     
-    for point in input_dir:
-        min_dist = curr_dis = 598989898
-        nearest_node_index = 0
- 
-        if (point in output_list):
-            continue
-        for index, node in enumerate(output_list):
-            curr_dis = distance(input_dir[node], input_dir[point])
-            print("round {} trying node {} ".format(index, node ))
-            if curr_dis < min_dist:
-                min_dist = curr_dis
-                nearest_node_index = index
-        output_list.insert(nearest_node_index+1,point)
-        
-            
+    return cycle_lenght
+
--- testTSP.py
+++ testTSP.py
@@ -23,14 +23,20 @@ def test_dstance_big_numbers():
     destination = (981665,218777)
     assert(TSP.distance(origen, destination) == 196169)
     
+def test_nearest_node():
+    input_dir = {1:(0,0),2:(20,0),3:(20,10),4:(0,10)}
+    distance, node = TSP.nearest_node(3,input_dir,[4])
+    assert(2 == node)
+    assert(10 == distance)
+
+
+
 
 def test_tsp_impl():
     input_dir = {1:(0,0),2:(20,0),3:(20,10),4:(0,10)}
     output_list = []
-    TSP.tsp_impl(input_dir, output_list, start=1)
-    assert(output_list == [1, 4, 2, 3])
+    total_dist = TSP.tsp_impl(input_dir, output_list, start=1)
+    assert(output_list == [1, 4, 3, 2])
 
     
-
-    
- 
+test_tsp_impl()
